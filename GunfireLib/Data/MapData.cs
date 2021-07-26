e3f75976ded81dae112fed315d46d54de3265c8d
ardittristan <ardittristan@gmail.com>
2021-07-26 23:22:51 +0200

    Improve translations

--- GunfireLib/Data/MapData.cs
+++ GunfireLib/Data/MapData.cs
@@ -1,4 +1,6 @@
 ﻿using StringList = System.Collections.Generic.List<string>;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.IO;
 using DataHelper;
 using FileParser;
@@ -13,6 +15,7 @@ namespace GunfireLib.Data
 
         private static StringList mapNameList = new StringList();
         private static StringList devMapNameList = new StringList();
+        private static readonly StringList fullDevMapNameList = new StringList();
 
         internal static void Setup()
         {
@@ -23,7 +26,7 @@ namespace GunfireLib.Data
                 SetupMapNameStore();
                 foreach (KeyValuePair<string, levelconfigdataclass> item in levelList)
                 {
-                    HandleMapNameStore("[" + item.Value.Name + "]", string.IsNullOrWhiteSpace(item.Value.DevName) ? "" : "[" + item.Value.DevName + "]");
+                    HandleMapNameStore(item.Value.Name, string.IsNullOrWhiteSpace(item.Value.DevName) ? "" : item.Value.DevName);
                 }
             }
         }
@@ -38,6 +41,11 @@ namespace GunfireLib.Data
                 IParsedFile file = new ParsedFile(Path.Combine(GunfireLib.libConfigDirectory, "mapNameList.txt"));
                 mapNameList = file.ToList<string>();
             }
+            if (File.Exists(Path.Combine(GunfireLib.libConfigDirectory, "devMapNameList.txt")))
+            {
+                IParsedFile file = new ParsedFile(Path.Combine(GunfireLib.libConfigDirectory, "devMapNameList.txt"));
+                devMapNameList = file.ToList<string>();
+            }
         }
 
         private static void SaveMapNameStore()
@@ -50,19 +58,41 @@ namespace GunfireLib.Data
             {
                 File.WriteAllLines(Path.Combine(GunfireLib.libConfigDirectory, "devMapNameList.txt"), devMapNameList);
             }
+            if (fullDevMapNameList.Count > 0)
+            {
+                File.WriteAllLines(Path.Combine(GunfireLib.libConfigDirectory, "fullDevMapNameList.txt"), fullDevMapNameList);
+            }
         }
 
         private static void HandleMapNameStore(string name, string devName)
         {
             if (GunfireLib.fileLog)
             {
+                name = "[" + name + "]";
                 if (!mapNameList.Contains(name))
                 {
                     mapNameList.Add(name);
                 }
-                if (!devMapNameList.Contains(devName))
+
+                devName = devName.Replace("（", "(");
+                devName = devName.Replace("）", ")");
+                StringList devMatches = Regex.Matches(devName, "[^\x00-\x7F]+")
+                                                .OfType<Match>()
+                                                .Select(m => m.Groups[0].Value)
+                                                .ToList();
+                foreach(string DevName in devMatches)
                 {
-                    devMapNameList.Add(devName);
+                    string ParsedDevName = "[" + DevName + "]";
+                    if (!devMapNameList.Contains(ParsedDevName))
+                    {
+                        devMapNameList.Add(ParsedDevName);
+                    }
+                }
+
+                devName = "[" + devName + "]";
+                if (!fullDevMapNameList.Contains(devName))
+                {
+                    fullDevMapNameList.Add(devName);
                 }
             }
         }
--- GunfireLib/Utils/DataHelpers.cs
+++ GunfireLib/Utils/DataHelpers.cs
@@ -1,4 +1,10 @@
-﻿using DataHelper;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using DataHelper;
+using GunfireLib.Data;
+using UnhollowerBaseLib;
 
 namespace GunfireLib.Utils
 {
@@ -6,12 +12,45 @@ namespace GunfireLib.Utils
     {
         public static string GetEnglishName(this levelconfigdataclass baseClass)
         {
-            return baseClass.Name;
+            if (string.IsNullOrWhiteSpace(baseClass.Name)) return "";
+            try
+            {
+                return MapData.MapNames[baseClass.Name];
+            }
+            catch (Exception ex) when (ex is Il2CppException || ex is KeyNotFoundException)
+            {
+                return baseClass.Name;
+            }
         }
 
         public static string GetEnglishDevName(this levelconfigdataclass baseClass)
         {
-            return baseClass.DevName;
+            if (string.IsNullOrWhiteSpace(baseClass.DevName)) return "";
+            try
+            {
+                return MapData.DevMapNames[baseClass.DevName];
+            }
+            catch (Exception ex) when (ex is Il2CppException || ex is KeyNotFoundException)
+            {
+                string DevName = baseClass.DevName;
+
+                DevName = DevName.Replace("（", "(");
+                DevName = DevName.Replace("）", ")");
+                List<string> matches = Regex.Matches(DevName, "[^\x00-\x7F]+")
+                                                .OfType<Match>()
+                                                .Select(m => m.Groups[0].Value)
+                                                .ToList();
+
+                foreach (string match in matches)
+                {
+                    try
+                    {
+                        DevName = DevName.Replace(match, MapData.DevMapNames[match]);
+                    }
+                    catch (Exception exc) when (exc is Il2CppException || exc is KeyNotFoundException) { };
+                }
+                return DevName;
+            }
         }
     }
 }
